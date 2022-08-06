8fe9ea37b43d60fbc146336647b0cd14ab230eef
Tristan Tornatore <tristan@revontuli.fr>
2022-08-06 19:55:04 +0200

    Fix error on build `'`
    I've use for a function `'` in h1 element. I've to change with &apos; to resolve this issue.

--- pages/blog/[articles].js
+++ pages/blog/[articles].js
@@ -1,45 +1,46 @@
-import React from 'react'
-
+import React from "react";
 
 export default function articles(props) {
-  if(!props.articleEnCours){
-    return <h1>Chargement de l'article</h1>
+  if (!props.articleEnCours) {
+    return <h1>Chargement de l&apos;article</h1>;
   }
   return (
     <main className="container">
-        <h1>{props.articleEnCours.title}</h1>
-        <p>{props.articleEnCours.body}</p>
+      <h1>{props.articleEnCours.title}</h1>
+      <p>{props.articleEnCours.body}</p>
     </main>
-  )
+  );
 }
 
-export async function getStaticProps(context){
-  const slug = context.params.articles
-  const data = await fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json())
-  const articleEnCours = data.find(item => item.title === slug)
+export async function getStaticProps(context) {
+  const slug = context.params.articles;
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts').then(
+    (response) => response.json()
+  );
+  const articleEnCours = data.find((item) => item.title === slug);
 
-  if(!articleEnCours){
-    return{
+  if (!articleEnCours) {
+    return {
       notFound: true,
-    }
+    };
   }
 
-  return{
+  return {
     props: {
-      articleEnCours
-    }
-  }
-
+      articleEnCours,
+    },
+  };
 }
 
-export async function getStaticPaths(){
-  const data = await fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json())
-  const paths = data.map(item => ({
-    params: {articles: item.title}
-  }
-  ))
+export async function getStaticPaths() {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts').then(
+    (response) => response.json()
+  );
+  const paths = data.map((item) => ({
+    params: { articles: item.title },
+  }));
   return {
     paths,
     fallback: true,
-  }
+  };
 }
